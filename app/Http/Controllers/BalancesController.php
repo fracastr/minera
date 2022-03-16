6e7906bf8c03571dda80ecdbded61f200d48dead
francisco castro <fr.castr@gmail.com>
2022-03-15 21:35:36 -0300

    Update BalancesController.php

--- app/Http/Controllers/BalancesController.php
+++ app/Http/Controllers/BalancesController.php
@@ -106,9 +106,6 @@ class BalancesController extends Controller
         $data = json_decode($response->getBody()->getContents());
         foreach ($data as $key => &$value) {
             // $value = json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $value), true );
-            if($key == 'datos_entrada'){
-                dd($value);
-            }
             $value = json_decode($value, true);
 
         }
