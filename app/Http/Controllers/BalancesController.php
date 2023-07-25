9207d50a40b8f894f528c4a258e61d984e125bae
Fernando Ureta <fernandoureta1@gmail.com>
2023-07-25 03:27:40 -0400

    Up
    u

--- app/Http/Controllers/BalancesController.php
+++ app/Http/Controllers/BalancesController.php
@@ -124,10 +124,10 @@ class BalancesController extends Controller
             $object_mediciones = array();
             if(sizeof($value_mediciones) == 4){
                 $object_mediciones['Flujos'] = $flujos[$key_mediciones];
-                $object_mediciones['TMS medido'] = number_format($value_mediciones[0], 10, '.', '');
-                $object_mediciones['TMS balance'] = number_format($value_mediciones[1], 10, '.', '');
-                $object_mediciones['Fet [%] Medido'] = number_format($value_mediciones[2], 4);
-                $object_mediciones['Fet [%] Balance'] = number_format($value_mediciones[3], 4);
+                $object_mediciones['TMS medido'] = number_format($value_mediciones[0], 0, '.', '');
+                $object_mediciones['TMS balance'] = number_format($value_mediciones[1], 0, '.', '');
+                $object_mediciones['Fet [%] Medido'] = number_format($value_mediciones[2], 2);
+                $object_mediciones['Fet [%] Balance'] = number_format($value_mediciones[3], 2);
             }
             else if(sizeof($value_mediciones) == 6){
                 $object_mediciones['Flujos'] = $flujos[$key_mediciones];
