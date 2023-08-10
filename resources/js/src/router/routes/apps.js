87e9d3a47e31a7dc91434e2a618863d4fed62271
francisco castro <fr.castr@gmail.com>
2023-08-10 02:18:53 -0400

    update files deleted for sonarqube report

--- resources/js/src/router/routes/apps.js
+++ resources/js/src/router/routes/apps.js
@@ -8,43 +8,7 @@ export default [
   // *===============================================---*
   // *--------- EMAIL & IT'S FILTERS N LABELS -------------------------------*
   // *===============================================---*
-  {
-    path: '/apps/email',
-    name: 'apps-email',
-    component: () => import('@/views/apps/email/Email.vue'),
-    meta: {
-      contentRenderer: 'sidebar-left',
-      contentClass: 'email-application',
-    },
-  },
-  {
-    path: '/apps/email/:folder',
-    name: 'apps-email-folder',
-    component: () => import('@/views/apps/email/Email.vue'),
-    meta: {
-      contentRenderer: 'sidebar-left',
-      contentClass: 'email-application',
-      navActiveLink: 'apps-email',
-    },
-    beforeEnter(to, _, next) {
-      if (['sent', 'draft', 'starred', 'spam', 'trash'].includes(to.params.folder)) next()
-      else next({ name: 'error-404' })
-    },
-  },
-  {
-    path: '/apps/email/label/:label',
-    name: 'apps-email-label',
-    component: () => import('@/views/apps/email/Email.vue'),
-    meta: {
-      contentRenderer: 'sidebar-left',
-      contentClass: 'email-application',
-      navActiveLink: 'apps-email',
-    },
-    beforeEnter(to, _, next) {
-      if (['personal', 'company', 'important', 'private'].includes(to.params.label)) next()
-      else next({ name: 'error-404' })
-    },
-  },
+  
 
   // *===============================================---*
   // *--------- TODO & IT'S FILTERS N TAGS ---------------------------------------*
