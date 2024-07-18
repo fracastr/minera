caf81fb15225dfa6d98cdb32cc98889111f34b27
francisco castro <fr.castr@gmail.com>
2024-07-18 01:53:06 -0400

    add

---
+++ app/Policies/MyCustomPolicy.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Policies;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Basic;
+
+class MyCustomPolicy extends Basic
+{
+    public function configure()
+    {
+        $this
+        ->addDirective(Directive::SCRIPT, 'self')
+        ->addDirective(Directive::STYLE, 'self')
+        ->addNonceForDirective(Directive::SCRIPT)
+        ->addNonceForDirective(Directive::STYLE);
+    }
+}
