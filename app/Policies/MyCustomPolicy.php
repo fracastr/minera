8cd1e47711ae43b1d8d8bae96cf3809d4d240734
francisco castro <fr.castr@gmail.com>
2024-07-18 01:56:27 -0400

    Update MyCustomPolicy.php

--- app/Policies/MyCustomPolicy.php
+++ app/Policies/MyCustomPolicy.php
@@ -2,16 +2,62 @@
 namespace App\Policies;
 
 use Spatie\Csp\Directive;
+use Spatie\Csp\Keyword;
 use Spatie\Csp\Policies\Basic;
 
 class MyCustomPolicy extends Basic
 {
     public function configure()
     {
-        $this
-        ->addDirective(Directive::SCRIPT, 'self')
-        ->addDirective(Directive::STYLE, 'self')
-        ->addNonceForDirective(Directive::SCRIPT)
-        ->addNonceForDirective(Directive::STYLE);
+        parent::configure();
+
+
+        //Styles
+        $this->addDirective(Directive::STYLE, [
+            'cdnjs.cloudflare.com',
+            'fonts.googleapis.com',
+            'cdn.datatables.net',
+            'cdn.jsdelivr.net',
+            'unsafe-inline'
+
+        ]);
+
+        //Fonts
+        $this->addDirective(Directive::FONT, [
+            'fonts.gstatic.com',
+            'cdnjs.cloudflare.com',
+            Keyword::SELF
+
+        ]);
+
+        //Script
+        $this->addDirective(Directive::SCRIPT, [
+            'cdn.datatables.net',
+            'cdn.jsdelivr.net',
+            'cdnjs.cloudflare.com',
+            'connect.facebook.net',
+            'www.google.com',
+            'www.gstatic.com',
+
+
+        ]);
+
+        //Image
+        $this->addDirective(Directive::IMG, [
+            Keyword::SELF,
+            Keyword::UNSAFE_INLINE,
+            'data:'
+
+        ]);
+
+        //iFrame
+        $this->addDirective(Directive::FRAME, [
+            'go.crisp.chat',
+            'www.google.com'
+        ]);
+
+//        //Inline Styles,Script
+        $this->addNonceForDirective(Directive::SCRIPT);
+        $this->addNonceForDirective(Directive::STYLE);
     }
 }
