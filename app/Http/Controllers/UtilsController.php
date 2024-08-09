216dc8cfc538b8e54abccc0f7b52e0068e0a42f5
francisco castro <fr.castr@gmail.com>
2024-08-09 13:56:24 -0400

    Revert "add notepath env"
    This reverts commit cbd28b0868cb89f2f909c0b51a6109426d9567a6.

--- app/Http/Controllers/UtilsController.php
+++ app/Http/Controllers/UtilsController.php
@@ -141,8 +141,7 @@ class UtilsController extends Controller
         $data_extra = json_encode($data_response->data_extra);
         $command = $public . '/excelnode.js';
         $filename = '';
-        $nodepath = env('NODEPATH');
-        $process = new Process([$nodepath, $command, $data, $data_extra, $datos_entrada_id, $public, $public . '/' . $arr_files[$proceso_id], $storage]);
+        $process = new Process(['/usr/bin/node', $command, $data, $data_extra, $datos_entrada_id, $public, $public . '/' . $arr_files[$proceso_id], $storage]);
         $process->run();
 
         // executes after the command finishes
