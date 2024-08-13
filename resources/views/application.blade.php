0c7a7b1d5a01c4d72bc98b7dd6902132fe88c730
francisco castro <fr.castr@gmail.com>
2024-08-13 19:41:24 -0400

    Revert "test waf"
    This reverts commit f169e35fa8a3236f433d733daa7f4e195841aef8.

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -6,7 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="cloudbriclabs-verification" content="47cee4c7dec6386ad41d0070bb957285a8733877e9c969532d8b0c2b0701fc72" />
   <!-- <link rel="icon" href="<%= BASE_URL %>favicon.ico"> -->
 
   <title>Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template</title>
