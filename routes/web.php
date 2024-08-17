3278b2befcff2157f574c8099ea6803c50e5e19d
francisco castro <fr.castr@gmail.com>
2024-08-17 13:45:38 -0400

    add test route to google drive api

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\BalancesController;
 use App\Http\Controllers\UtilsController;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,6 @@ Route::get('balances/getValles/{user_id}', [UtilsController::class, 'getValles']
 Route::get('balances/getProcesos/{valle_id}', [UtilsController::class, 'getProcesos']);
 Route::get('balances/getExcel/{datos_entrada_id}/{proceso_id}', [UtilsController::class, 'getExcel']);
 Route::get('/{any}', [ApplicationController::class, 'index'])->where('any', '.*');
+Route::get('test', function() {
+    Storage::disk('google')->put('test.txt', 'Hello World');
+});
