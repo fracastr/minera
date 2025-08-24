fe959d7278a3b45e6f437cc9f0412407018c089b
fracastr <fr.castr@gmail.com>
2025-08-24 15:13:31 -0400

    Update BalancesController.php

--- app/Http/Controllers/BalancesController.php
+++ app/Http/Controllers/BalancesController.php
@@ -294,8 +294,12 @@ class BalancesController extends Controller
     public function import(Request $request)
     {
         try {
-            $path = $request->file('file')->store('public');
-            $path = '/home/ubuntu/minera/storage/app/'. $path;
+            if (app()->environment('local')) {
+                $path = "/home/ubuntu/minera/storage/app/public/I6lpMJiywp3X2ifsT7M3ujh9WWsxPeZgarZbK4Jp.xlsx";
+            } else {
+                $path = $request->file('file')->store('public');
+                $path = '/home/ubuntu/minera/storage/app/'. $path;
+            }
 
         $proceso_id = $request->proceso_id;
         $proceso = Procesos::find($proceso_id);
